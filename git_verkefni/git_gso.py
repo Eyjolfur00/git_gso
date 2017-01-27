753030fd878c90adfb3c410838b543b8fa3f5c4f
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2017-01-27 14:05:27 +0000

    3.punktur

--- git_verkefni/git_gso.py
+++ git_verkefni/git_gso.py
@@ -7,4 +7,15 @@ file.write("tilgangslaus texti ""\n")
 file.close()
 
 
+nafn = input("Sláðu inn nafn : ")
+kennitala = input("Sláðu inn kennitölu : ")
+aldur = int(input("Sláðu inn aldur"))
+
+file = open(nafnskra,"a")
+file.write(nafn+"\n")
+file.write(kennitala+"\n")
+file.write(str(aldur))
+file.close()
+
+
 
