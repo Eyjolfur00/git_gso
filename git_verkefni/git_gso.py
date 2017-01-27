d82d8ef02ed85239b77e4cd0acfd80344695d9e8
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2017-01-27 14:03:53 +0000

    1.punktur

--- git_verkefni/git_gso.py
+++ git_verkefni/git_gso.py
@@ -1,16 +1,6 @@
 #Eyjólfur J.
-
-file = open("gso.txt","w")
-
-file.write("tilgangslaus texti ")
-file.close()
+skra = input("Skráðu inn nafn skráar")
+nafnskra = skra +".txt"
 
 
-nafn = str(input("Sláðu inn nafn : "))
-kennitala = input("Sláðu inn kennitölu : ")
-print("-----------------------------")
-print("Nafn þitt er",nafn)
-print("Kennitalan þín er : ",kennitala)
-print("Takk fyrir  :)")
-print("-----------------------------")
-file = open("gso.txt","r")
+
