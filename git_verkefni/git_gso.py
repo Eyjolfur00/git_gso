7e464a15ff19ee96302c6b7ecc16ec2a9dd63572
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2017-01-27 13:39:50 +0000

    3.punktur

--- git_verkefni/git_gso.py
+++ git_verkefni/git_gso.py
@@ -5,3 +5,12 @@ file = open("gso.txt","w")
 file.write("tilgangslaus texti ")
 file.close()
 
+
+nafn = str(input("Sláðu inn nafn : "))
+kennitala = input("Sláðu inn kennitölu : ")
+print("-----------------------------")
+print("Nafn þitt er",nafn)
+print("Kennitalan þín er : ",kennitala)
+print("Takk fyrir  :)")
+print("-----------------------------")
+file = open("gso.txt","r")
